9d17c409ca0c5d94684445573724172f28d4c1a3
els-lartee <elsielartey0@gmail.com>
2025-09-25 17:56:10 +0000

    part 1

--- settings/core.php
+++ settings/core.php
@@ -6,17 +6,25 @@ session_start();
 //for header redirection
 ob_start();
 
-//funtion to check for login
-if (!isset($_SESSION['id'])) {
-    header("Location: ../Login/login_register.php");
+//function to check for login
+if (!isset($_SESSION['user_id'])) {
+    header("Location: ../login/login_register.php");
     exit;
 }
 
+//function to check if user is logged in
+function is_logged_in() {
+    return isset($_SESSION['user_id']) && !empty($_SESSION['user_id']);
+}
 
 //function to get user ID
-
+function get_user_id() {
+    return $_SESSION['user_id'] ?? null;
+}
 
 //function to check for role (admin, customer, etc)
+function is_admin() {
+    return isset($_SESSION['user_role']) && $_SESSION['user_role'] == 1;
 
 
 
