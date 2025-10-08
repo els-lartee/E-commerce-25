e44786918c387978327dbf752a352404c4f89624
els-lartee <elsielartey0@gmail.com>
2025-10-08 19:04:49 +0000

    Test

--- login/register.php
+++ login/register.php
@@ -46,6 +46,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="../js/register.js"></script>
     <style>
         /* your same CSS styles here */
